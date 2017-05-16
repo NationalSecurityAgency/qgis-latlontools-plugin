36013f531941642339952ce7fc889cf0ea0897cd
C Hamilton <publisher@solarviews.com>
2017-05-16 16:17:33 -0400

    test

--- copyLatLonTool.py
+++ copyLatLonTool.py
@@ -1,10 +1,10 @@
-from PyQt4.QtCore import Qt, pyqtSignal
-from PyQt4.QtGui import QApplication
-from qgis.core import QgsCoordinateTransform, QgsPoint
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtWidgets import QApplication
+from qgis.core import QgsCoordinateTransform
 from qgis.gui import QgsMapTool, QgsMessageBar
 
-from LatLon import LatLon
-import mgrs
+from .LatLon import LatLon
+from . import mgrs
 
 class CopyLatLonTool(QgsMapTool):
     '''Class to interact with the map canvas to capture the coordinate
