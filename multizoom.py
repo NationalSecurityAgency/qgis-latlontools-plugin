5513437a99bfbed266d84d6a8f0acafbfe2a9842
C Hamilton <publisher@solarviews.com>
2017-07-12 16:31:43 -0400

    fix markers from not being removed

--- multizoom.py
+++ multizoom.py
@@ -141,8 +141,10 @@ class MultiZoomWidget(QDockWidget, FORM_CLASS):
             QMessageBox.Yes, QMessageBox.No)
 
         if reply == QMessageBox.Yes:
+            self.resultsTable.blockSignals(True)
             self.removeMarkers()
             self.resultsTable.setRowCount(0)
+            self.resultsTable.blockSignals(False)
         
     def showSettings(self):
         self.settings.showTab(3)
@@ -185,15 +187,6 @@ class MultiZoomWidget(QDockWidget, FORM_CLASS):
             if item.marker is not None:
                 self.canvas.scene().removeItem(item.marker)
                 item.marker = None
-        
-    def removeMarker(self, row):
-        rowcnt = self.resultsTable.rowCount()
-        if row >= rowcnt:
-            return
-        item = self.resultsTable.item(row, 0).data(Qt.UserRole)
-        if item.marker is not None:
-            self.canvas.scene().removeItem(item.marker)
-            item.marker = None
         
     def openDialog(self):
         filename = QFileDialog.getOpenFileName(None, "Input File", 
@@ -262,12 +255,19 @@ class MultiZoomWidget(QDockWidget, FORM_CLASS):
             QMessageBox.Yes, QMessageBox.No)
 
         if reply == QMessageBox.Yes:
+            # Blocking the signals is necessary to prevent the signals replacing
+            # the marker before it is completely removed.
+            self.resultsTable.blockSignals(True)
             for row in sorted(indices, reverse=True):
                 # Remove the marker from the map
-                self.removeMarker(row)
+                item = self.resultsTable.item(row, 0).data(Qt.UserRole)
+                if item.marker is not None:
+                    self.canvas.scene().removeItem(item.marker)
+                    item.marker = None
                 # Then remove the location from the table
                 self.resultsTable.removeRow(row)
             
+            self.resultsTable.blockSignals(False)
             self.resultsTable.clearSelection()
     
     def addSingleCoord(self):
