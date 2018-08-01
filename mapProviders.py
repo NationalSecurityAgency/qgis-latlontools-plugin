773609b52be9617449204d68e4a83aeff0274c80
C <adenaculture@gmail.com>
2018-08-01 11:19:05 -0400

    minor change

--- mapProviders.py
+++ mapProviders.py
@@ -7,8 +7,8 @@ MAP_PROVIDERS = [
 ['Bing Aerial', 'https://www.bing.com/maps?cp=@LAT@~@LON@&lvl=@Z@&style=a', 'https://www.bing.com/maps?cp=@LAT@~@LON@&lvl=@Z@&style=a&sp=point.@LAT@_@LON@_QGIS%20Point'],
 ['MapQuest Map', 'https://www.mapquest.com/latlng/@LAT@,@LON@?centerOnResults=1&zoom=@Z@', 'https://www.mapquest.com/latlng/@LAT@,@LON@?centerOnResults=1&zoom=@Z@'],
 ['MapQuest Aerial', 'https://www.mapquest.com/latlng/@LAT@,@LON@?centerOnResults=1&maptyp=sat&zoom=@Z@', 'https://www.mapquest.com/latlng/@LAT@,@LON@?centerOnResults=1&maptyp=sat&zoom=@Z@'],
-['Mapillary Street', 'https://www.mapillary.com/app/?lat=@LAT@&lng=@LON@&z=@Z@&menu=false&focus=map&mapStyle=mapbox_streets', 'https://www.mapillary.com/app/?lat=@LAT@&lng=@LON@&z=@Z@&menu=false&focus=map&mapStyle=mapbox_streets'],
-['Mapillary Aerial', 'https://www.mapillary.com/app/?lat=@LAT@&lng=@LON@&z=@Z@&menu=false&focus=map&mapStyle=mapbox_satellite', 'https://www.mapillary.com/app/?lat=@LAT@&lng=@LON@&z=@Z@&menu=false&focus=map&mapStyle=mapbox_satellite']
+['Mapillary Street', 'https://www.mapillary.com/app/?lat=@LAT@&lng=@LON@&z=@Z@&mapStyle=mapbox_streets', 'https://www.mapillary.com/app/?lat=@LAT@&lng=@LON@&z=@Z@&mapStyle=mapbox_streets'],
+['Mapillary Aerial', 'https://www.mapillary.com/app/?lat=@LAT@&lng=@LON@&z=@Z@&mapStyle=mapbox_satellite', 'https://www.mapillary.com/app/?lat=@LAT@&lng=@LON@&z=@Z@&mapStyle=mapbox_satellite']
 ]
 
 def mapProviderNames():
