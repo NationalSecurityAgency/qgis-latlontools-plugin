69afad5dd9e25fc802d6d66d9c47bc6406cbe868
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-08 09:53:27 -0700

    Fixed PYTHON_E231 issues in mapProviders.py

--- mapProviders.py
+++ mapProviders.py
@@ -1,6 +1,6 @@
 
 MAP_PROVIDERS = [
-['OSM', 'http://www.openstreetmap.org/#map=@Z@/@LAT@/@LON@','http://www.openstreetmap.org/?mlat=@LAT@&mlon=@LON@#map=@Z@/@LAT@/@LON@'],
+['OSM', 'http://www.openstreetmap.org/#map=@Z@/@LAT@/@LON@', 'http://www.openstreetmap.org/?mlat=@LAT@&mlon=@LON@#map=@Z@/@LAT@/@LON@'],
 ['Google Map', 'https://www.google.com/maps/@@LAT@,@LON@,@Z@z', 'https://www.google.com/maps/place/@LAT@,@LON@/@@LAT@,@LON@,@Z@z'],
 ['Google Aerial', 'https://www.google.com/maps/@@LAT@,@LON@,@Z@z/data=!3m1!1e3', 'https://www.google.com/maps/place/@LAT@,@LON@/@@LAT@,@LON@,@Z@z/data=!3m1!1e3'],
 ['Bing Map', 'https://www.bing.com/maps?cp=@LAT@~@LON@&lvl=@Z@', 'https://www.bing.com/maps?cp=@LAT@~@LON@&lvl=@Z@&sp=point.@LAT@_@LON@_QGIS%20Point'],
