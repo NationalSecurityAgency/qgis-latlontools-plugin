f5ae226ff9b354c6ceb2cd016d89d5d3d97395fa
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-08 09:53:19 -0700

    Fixed PYTHON_E231 issues in showOnMapTool.py

--- showOnMapTool.py
+++ showOnMapTool.py
@@ -1,7 +1,7 @@
 from qgis.PyQt.QtCore import Qt, QUrl
 from qgis.PyQt.QtGui import *
 from qgis.PyQt.QtWidgets import *
-from qgis.core import Qgis,QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsProject
+from qgis.core import Qgis, QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsProject
 from qgis.gui import QgsMapToolEmitPoint, QgsVertexMarker
 from .util import *
 from .settings import settings
@@ -47,16 +47,16 @@ class ShowOnMapTool(QgsMapToolEmitPoint):
         lat = pt4326.y()
         lon = pt4326.x()
         if settings.googleEarthMapProvider():
-            f = tempfile.NamedTemporaryFile(mode='w',suffix = ".kml",delete=False)
+            f = tempfile.NamedTemporaryFile(mode='w', suffix = ".kml", delete=False)
             f.write('<?xml version="1.0" encoding="UTF-8"?>')
             f.write('<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">')
             f.write('<Document>')
             f.write('   <name>QGIS Location</name>')
-            f.write('   <description>{:.8f}, {:.8f}</description>'.format(lon,lat))
+            f.write('   <description>{:.8f}, {:.8f}</description>'.format(lon, lat))
             f.write('   <Placemark>')
             f.write('       <name>QGIS Location</name>')
             f.write('       <Point>')
-            f.write('           <coordinates>{:.8f},{:.8f},0</coordinates>'.format(lon,lat))
+            f.write('           <coordinates>{:.8f},{:.8f},0</coordinates>'.format(lon, lat))
             f.write('       </Point>')
             f.write('   </Placemark>')
             f.write('</Document>')
@@ -66,12 +66,12 @@ class ShowOnMapTool(QgsMapToolEmitPoint):
                 os.startfile(f.name)
             else:
                 webbrowser.open(f.name)
-            self.iface.messageBar().pushMessage("", "Viewing Coordinate %f,%f in Google Earth" % (lat,lon), level=Qgis.Info, duration=3)
+            self.iface.messageBar().pushMessage("", "Viewing Coordinate %f,%f in Google Earth" % (lat, lon), level=Qgis.Info, duration=3)
         else:
             mapprovider = settings.getMapProviderString(lat, lon)
             url = QUrl(mapprovider).toString()
             webbrowser.open(url, new=2)
-            self.iface.messageBar().pushMessage("", "Viewing Coordinate %f,%f in external map" % (lat,lon), level=Qgis.Info, duration=3)
+            self.iface.messageBar().pushMessage("", "Viewing Coordinate %f,%f in external map" % (lat, lon), level=Qgis.Info, duration=3)
     
     def removeMarker(self):
         if self.marker is not None:
