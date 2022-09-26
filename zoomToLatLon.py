c0d119c163d97939316357b467c3c8de25d2677e
C Hamilton <hamiltoncj@users.noreply.github.com>
2022-09-26 18:08:04 +0000

    Add H3 and zoom to area bounding box

--- zoomToLatLon.py
+++ zoomToLatLon.py
@@ -2,22 +2,24 @@ import os
 import re
 
 from qgis.PyQt.uic import loadUiType
-from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtGui import QIcon, QColor
 from qgis.PyQt.QtWidgets import QDockWidget, QApplication
 from qgis.PyQt.QtCore import QTextCodec
-from qgis.gui import QgsVertexMarker
-from qgis.core import Qgis, QgsJsonUtils, QgsWkbTypes
+from qgis.gui import QgsRubberBand
+from qgis.core import Qgis, QgsJsonUtils, QgsWkbTypes, QgsPointXY, QgsGeometry, QgsCoordinateTransform, QgsProject, QgsRectangle
 from .util import epsg4326, parseDMSString
-from .settings import CoordOrder
+from .settings import settings, CoordOrder, H3_INSTALLED
 from .utm import isUtm, utm2Point
 from .ups import isUps, ups2Point
-# import traceback
+import traceback
 
 from . import mgrs
 from . import olc
 from . import geohash
-from .maidenhead import maidenGridCenter
+from .maidenhead import maidenGrid
 from . import georef
+if H3_INSTALLED:
+    import h3
 
 FORM_CLASS, _ = loadUiType(os.path.join(
     os.path.dirname(__file__), 'ui/zoomToLatLon.ui'))
@@ -29,7 +31,6 @@ class ZoomToLatLon(QDockWidget, FORM_CLASS):
         super(ZoomToLatLon, self).__init__(parent)
         self.setupUi(self)
         self.canvas = iface.mapCanvas()
-        self.marker = None
         self.clipboard = QApplication.clipboard()
         self.zoomToolButton.setIcon(QIcon(':/images/themes/default/mActionZoomIn.svg'))
         self.clearToolButton.setIcon(QIcon(':/images/themes/default/mIconClearText.svg'))
@@ -44,6 +45,17 @@ class ZoomToLatLon(QDockWidget, FORM_CLASS):
         self.iface = iface
         self.coordTxt.returnPressed.connect(self.zoomToPressed)
         self.canvas.destinationCrsChanged.connect(self.crsChanged)
+        
+        self.marker = QgsRubberBand(self.canvas, QgsWkbTypes.PointGeometry)
+        self.marker.setColor(settings.markerColor)
+        self.marker.setStrokeColor(settings.markerColor)
+        self.marker.setWidth(2)
+        self.marker.setIconSize(18)
+        self.marker.setIcon(QgsRubberBand.ICON_CROSS)
+        
+        self.line_marker = QgsRubberBand(self.canvas, QgsWkbTypes.LineGeometry)
+        self.line_marker.setWidth(2)
+        self.line_marker.setColor(settings.gridColor)
         self.configure()
 
     def showEvent(self, e):
@@ -75,6 +87,8 @@ class ZoomToLatLon(QDockWidget, FORM_CLASS):
             self.label.setText("Enter Plus Codes")
         elif self.settings.zoomToProjIsGeohash():
             self.label.setText("Enter Geohash")
+        elif self.settings.zoomToProjIsH3():
+            self.label.setText("Enter H3 geohash")
         elif self.settings.zoomToProjIsStandardUtm():
             self.label.setText("Enter Standard UTM")
         elif self.settings.zoomToProjIsMaidenhead():
@@ -104,7 +118,7 @@ class ZoomToLatLon(QDockWidget, FORM_CLASS):
                 # if it is not a valid MGRS coordinate
                 text2 = re.sub(r'\s+', '', str(text))  # Remove all white space
                 lat, lon = mgrs.toWgs(text2)
-                return(lat, lon, epsg4326)
+                return(lat, lon, None, epsg4326)
 
             if self.settings.zoomToProjIsPlusCodes():
                 # A Plus Codes coordinate has been selected. This will result in an exception
@@ -112,25 +126,47 @@ class ZoomToLatLon(QDockWidget, FORM_CLASS):
                 coord = olc.decode(text)
                 lat = coord.latitudeCenter
                 lon = coord.longitudeCenter
-                return(lat, lon, epsg4326)
+                rect = QgsRectangle(coord.longitudeLo, coord.latitudeLo, coord.longitudeHi, coord.latitudeHi)
+                geom = QgsGeometry.fromRect(rect)
+                return(lat, lon, geom, epsg4326)
 
             if self.settings.zoomToProjIsStandardUtm():
                 # A Standard UTM coordinate has been selected. This will result in an exception
                 # if it is not a valid utm coordinate.
                 pt = utm2Point(text)
-                return(pt.y(), pt.x(), epsg4326)
+                return(pt.y(), pt.x(), None, epsg4326)
 
             if self.settings.zoomToProjIsGeohash():
                 # A Geohash coordinate has been selected. This will result in an exception
                 # if it is not a valid Geohash coordinate.
-                (lat, lon, lat_err, lon_err) = geohash.decode_exactly(text)
-                return(lat, lon, epsg4326)
+                (lat1, lat2, lon1, lon2) = geohash.decode_extent(text)
+                lat = (lat1 + lat2) / 2
+                lon = (lon1 + lon2) / 2
+                rect = QgsRectangle(lon1, lat1, lon2, lat2)
+                geom = QgsGeometry.fromRect(rect)
+                return(lat, lon, geom, epsg4326)
+
+            if self.settings.zoomToProjIsH3():
+                # An H3 coordinate has been selected. 
+                if not h3.h3_is_valid(text):
+                    raise ValueError('Invalid H3 Coordinate')
+                (lat, lon) = h3.h3_to_geo(text)
+                coords = h3.h3_to_geo_boundary(text)
+                pts = []
+                for p in coords:
+                    pt = QgsPointXY(p[1], p[0])
+                    pts.append(pt)
+                pts.append(pts[0])  # Close the polygon
+                geom = QgsGeometry.fromPolylineXY(pts)
+                return(lat, lon, geom, epsg4326)
 
             if self.settings.zoomToProjIsMaidenhead():
                 # A Maidenhead grid coordinate has been selected. This will result in an exception
                 # if it is not a valid maidenhead coordinate.
-                (lat, lon) = maidenGridCenter(text)
-                return(float(lat), float(lon), epsg4326)
+                (lat, lon, lat1, lon1, lat2, lon2) = maidenGrid(text)
+                rect = QgsRectangle(lon1, lat1, lon2, lat2)
+                geom = QgsGeometry.fromRect(rect)
+                return(float(lat), float(lon), geom, epsg4326)
 
             # Check for other formats
             if text[0] == '{':  # This may be a GeoJSON point
@@ -144,22 +180,22 @@ class ZoomToLatLon(QDockWidget, FORM_CLASS):
                 if geom.isEmpty() or (geom.wkbType() != QgsWkbTypes.Point):
                     raise ValueError('Invalid GeoJSON Geometry')
                 pt = geom.asPoint()
-                return(pt.y(), pt.x(), epsg4326)
+                return(pt.y(), pt.x(), None, epsg4326)
 
             # Check to see if it is standard UTM
             if isUtm(text):
                 pt = utm2Point(text)
-                return(pt.y(), pt.x(), epsg4326)
+                return(pt.y(), pt.x(), None, epsg4326)
 
             # Check to see if it is a UPS coordinate
             if isUps(text):
                 pt = ups2Point(text)
-                return(pt.y(), pt.x(), epsg4326)
+                return(pt.y(), pt.x(), None, epsg4326)
 
             # Check to see if it is a Georef coordinate
             try:
                 (lat, lon, prec) = georef.decode(text, False)
-                return(lat, lon, epsg4326)
+                return(lat, lon, None, epsg4326)
             except Exception:
                 pass
 
@@ -167,7 +203,7 @@ class ZoomToLatLon(QDockWidget, FORM_CLASS):
             try:
                 text2 = re.sub(r'\s+', '', str(text))
                 lat, lon = mgrs.toWgs(text2)
-                return(lat, lon, epsg4326)
+                return(lat, lon, None, epsg4326)
             except Exception:
                 pass
 
@@ -176,14 +212,14 @@ class ZoomToLatLon(QDockWidget, FORM_CLASS):
                 coord = olc.decode(text)
                 lat = coord.latitudeCenter
                 lon = coord.longitudeCenter
-                return(lat, lon, epsg4326)
+                return(lat, lon, None, epsg4326)
             except Exception:
                 pass
 
             # Check to see if it is a geohash string
             try:
                 (lat, lon, lat_err, lon_err) = geohash.decode_exactly(text)
-                return(lat, lon, epsg4326)
+                return(lat, lon, None, epsg4326)
             except Exception:
                 pass
 
@@ -200,12 +236,12 @@ class ZoomToLatLon(QDockWidget, FORM_CLASS):
                     srcCrs = self.canvas.mapSettings().destinationCrs()
                 else:
                     srcCrs = self.settings.zoomToCustomCRS()
-                return(lat, lon, srcCrs)
+                return(lat, lon, None, srcCrs)
 
             # We are left with either DMS or decimal degrees in one of the projections
             if self.settings.zoomToProjIsWgs84():
                 lat, lon = parseDMSString(text, self.settings.zoomToCoordOrder)
-                return(lat, lon, epsg4326)
+                return(lat, lon, None, epsg4326)
 
             # We are left with a non WGS 84 decimal projection
             coords = re.split(r'[\s,;:]+', text, 1)
@@ -221,27 +257,31 @@ class ZoomToLatLon(QDockWidget, FORM_CLASS):
                 srcCrs = self.canvas.mapSettings().destinationCrs()
             else:
                 srcCrs = self.settings.zoomToCustomCRS()
-            return(lat, lon, srcCrs)
+            return(lat, lon, None, srcCrs)
 
         except Exception:
+            traceback.print_exc()
             raise ValueError('Invalid Coordinates')
         
     def zoomToPressed(self):
         try:
             text = self.coordTxt.text().strip()
-            (lat, lon, srcCrs) = self.convertCoordinate(text)
+            (lat, lon, bounds, srcCrs) = self.convertCoordinate(text)
             pt = self.lltools.zoomTo(srcCrs, lat, lon)
+            self.marker.reset(QgsWkbTypes.PointGeometry)
+            self.marker.setColor(settings.markerColor)
             if self.settings.persistentMarker:
-                if self.marker is None:
-                    self.marker = QgsVertexMarker(self.canvas)
-                self.marker.setCenter(pt)
-                self.marker.setIconSize(18)
-                self.marker.setPenWidth(2)
-                self.marker.setIconType(QgsVertexMarker.ICON_CROSS)
-            elif self.marker is not None:
-                self.removeMarker()
+                self.marker.addPoint(pt)
+            self.line_marker.reset(QgsWkbTypes.LineGeometry)
+            self.line_marker.setColor(settings.gridColor)
+            if bounds and self.settings.showGrid:
+                canvas_crs = self.canvas.mapSettings().destinationCrs()
+                if srcCrs != canvas_crs:
+                    tr = QgsCoordinateTransform(srcCrs, canvas_crs, QgsProject.instance())
+                    bounds.transform(tr)
+                self.line_marker.addGeometry(bounds, None)
         except Exception:
-            # traceback.print_exc()
+            traceback.print_exc()
             self.iface.messageBar().pushMessage("", "Invalid Coordinate", level=Qgis.Warning, duration=2)
             return
 
@@ -251,9 +291,8 @@ class ZoomToLatLon(QDockWidget, FORM_CLASS):
         self.coordTxt.setText(text)
         
     def removeMarker(self):
-        if self.marker is not None:
-            self.canvas.scene().removeItem(self.marker)
-            self.marker = None
+        self.marker.reset(QgsWkbTypes.PointGeometry)
+        self.line_marker.reset(QgsWkbTypes.LineGeometry)
         self.coordTxt.clear()
 
     def showSettings(self):
