48f59478d4451bcaad55dfca8697098a7da884e5
C Hamilton <hamiltoncj@users.noreply.github.com>
2022-09-26 18:10:08 +0000

    Add maidenhead grid function

--- maidenhead.py
+++ maidenhead.py
@@ -52,6 +52,75 @@ def maidenGridCenter(maiden):
 
     return lat, lon
 
+def maidenGrid(maiden):
+    """
+        maiden is a string the is even and between 2 to 8 characters
+        It returns the center point of a maiden grid and throws an exception
+        on error
+    """
+    if not isinstance(maiden, str):
+        raise TypeError('Maidenhead locator must be a string')
+
+    maiden = maiden.strip().upper()
+
+    N = len(maiden)
+    if not 8 >= N >= 2 and N % 2 == 0:
+        raise ValueError('Maidenhead locator requires 2-8 characters, even number of characters')
+
+    Oa = ord('A')
+    lon = -180.
+    lat = -90.
+# %% first pair
+    isValid(maiden[0], 0)
+    isValid(maiden[1], 0)
+    lon += (ord(maiden[0])-Oa)*20
+    lat += (ord(maiden[1])-Oa)*10
+    if N == 2:
+        lon1 = lon
+        lat1 = lat
+        lon2 = lon + 20
+        lat2 = lat + 10
+        lon += 10
+        lat += 5
+# %% second pair
+    if N >= 4:
+        isValid(maiden[2], 1)
+        isValid(maiden[3], 1)
+        lon += int(maiden[2])*2
+        lat += int(maiden[3])*1
+    if N == 4:
+        lon1 = lon
+        lat1 = lat
+        lon2 = lon + 2
+        lat2 = lat + 1
+        lon += 1
+        lat += 0.5
+# %%
+    if N >= 6:
+        isValid(maiden[4], 2)
+        isValid(maiden[5], 2)
+        lon += (ord(maiden[4])-Oa) * 5./60
+        lat += (ord(maiden[5])-Oa) * 2.5/60
+    if N == 6:
+        lon1 = lon
+        lat1 = lat
+        lon2 = lon + 5./60
+        lat2 = lat + 2.5/60
+        lon += 5./120
+        lat += 2.5/120
+# %%
+    if N == 8:
+        lon += int(maiden[6]) * 5./600
+        lat += int(maiden[7]) * 2.5/600
+        lon1 = lon
+        lat1 = lat
+        lon2 = lon + 5./600
+        lat2 = lat + 2.5/600
+        lon += 5./1200
+        lat += 2.5/1200
+
+    return lat, lon, lat1, lon1, lat2, lon2
+
 def isValid(c, level):
     if level == 0:
         if not 'R' >= c >= 'A':
