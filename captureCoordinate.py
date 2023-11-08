1140efa24b42be82fdd294b5bcf26a733652f590
C Hamilton <hamiltoncj@users.noreply.github.com>
2023-11-08 14:59:26 +0000

    Add license

--- captureCoordinate.py
+++ captureCoordinate.py
@@ -1,3 +1,13 @@
+"""
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 from qgis.PyQt.QtCore import Qt, pyqtSignal
 from qgis.PyQt.QtGui import QColor
 from qgis.PyQt.QtCore import pyqtSlot
